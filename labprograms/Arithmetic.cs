635650f7a16878100cb95629fb19aabfe2914e15
SYEDHASEENA <syedhaseena766@gmail.com>
2022-07-24 17:57:03 +0530

    My first c#program

---
+++ labprograms/Arithmetic.cs
@@ -0,0 +1,25 @@
+/***************************************************************
+                alphabetacoder.com
+ C# program for addition, subtraction, multiplication, division
+****************************************************************/
+
+using System;
+
+
+
+    class Arithmetic
+    {  
+        static void Main(string[] args){
+        //declaration
+        int a=200;
+        int b=10;
+
+           Console.WriteLine("Addition: "+(a+b));
+            Console.WriteLine("Subtraction: "+(a-b));
+            Console.WriteLine("Multiplication: "+(a*b));
+            Console.WriteLine("Division: "+(a/b));
+            
+            
+            }
+    
+}
